46634ea05803dce2214cfe4f27f686ccac2d539b
ridzwan97 <ridzone97@gmail.com>
2018-10-26 06:19:22 +0800

    sql

---
+++ index.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE mydb;
+USE mydb;
+CREATE TABLE mytable ( id INT PRIMARY KEY, name VARCHAR(20) );
+INSERT INTO mytable VALUES ( 1, 'Will' );
+INSERT INTO mytable VALUES ( 2, 'Marry' );
+INSERT INTO mytable VALUES ( 3, 'Dean' );
+SELECT id, name FROM mytable WHERE id = 1;
+UPDATE mytable SET name = 'Willy' WHERE id = 1;
+SELECT id, name FROM mytable;
+DELETE FROM mytable WHERE id = 1;
+SELECT id, name FROM mytable;
+DROP DATABASE mydb;
+SELECT count(1) from mytable;
